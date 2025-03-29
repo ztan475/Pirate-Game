b33d6313a7ca6e78dbbbdebada5dab63c050edc1
HRKNE27 <harrisonnguyen0127@gmail.com>
2025-03-28 21:37:42 -0700

    fixed on trigger
    aaaaaaaaa

--- Assets/Scripts/Projectiles/GooSlowOverTime.cs
+++ Assets/Scripts/Projectiles/GooSlowOverTime.cs
@@ -7,17 +7,16 @@ public class GooSlowOverTime : MonoBehaviour
 {
     [SerializeField] private ProjectileStats projectileStats;
     [SerializeField] private float duration;
-    [Range(0,1)] private float slowAmount; 
+    [Range(0,1)] public float slowAmount; 
 
     private Dictionary<GameObject,float> enemyList = new Dictionary<GameObject,float>();
 
     private void Awake()
     {
-        InvokeRepeating("SlowOverTime", 0.5f, 1.0f);
         Destroy(gameObject, duration);
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
@@ -25,7 +24,7 @@ public class GooSlowOverTime : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
